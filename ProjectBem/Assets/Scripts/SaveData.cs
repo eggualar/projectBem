84e22f6925ac2c398b77dce7aec8b2938d7a1ef9
eggualar <ghkrbals@naver.com>
2024-03-20 14:02:31 +0900

    특성포인트 추가

--- ProjectBem/Assets/Scripts/SaveData.cs
+++ ProjectBem/Assets/Scripts/SaveData.cs
@@ -9,6 +9,9 @@ public class SaveData : MonoBehaviour
     public static SaveData instance;
     [HideInInspector]
     public int perk;
+    public float baseDamage;
+    public float baseSpeed;
+    public float baseMaxHp;
 
     private void Awake()
     {
@@ -24,12 +27,34 @@ public class SaveData : MonoBehaviour
 
     public void Reset()
     {
+        //퍽 개수 초기화
         PlayerPrefs.SetInt("SavePerk", 0);
         perk = 0;
+
+        //퍽 업글 초기화
+        PlayerPrefs.SetFloat("BaseDmg", 0);
+        PlayerPrefs.SetFloat("BaseSpd", 0);
+        PlayerPrefs.SetFloat("BaseMHp", 0);
     }
 
     public void GameStart()
     {
         SceneManager.LoadScene(0);
+    }
+
+
+    // 퍽 업그레이드 함수들
+    public void BaseDmgUpg()
+    {
+
+    }
+
+    public void BaseSpdUpg()
+    {
+
+    }
+    public void BaseMHpUpg()
+    {
+
     }
 }
